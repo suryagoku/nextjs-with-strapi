936f5ab7a4739401f2ee01e5264b206496aefec8
Gokul Surya <gokulsurya.gs@gmail.com>
2024-06-07 15:53:11 +0530

    feat: :sparkles: add sample home-page for frontend and backend
    Added home-page endpoint in backend with sample data. Integreated the home-page api in frontend.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,24 @@
 import { Button } from '@/components/ui/button'
 
-export default function Home() {
+const getStrapiData = async (path: string) => {
+  const baseUrl = 'http://localhost:1337/api'
+  try {
+    const response = await fetch(baseUrl + path, { cache: 'no-store' })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData('/home-page')
+  console.log(strapiData)
+  const { title, description } = strapiData.data.attributes
   return (
     <main className="container mx-auto py-6">
-      <Button>Our Cool Button</Button>
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
     </main>
   )
 }
