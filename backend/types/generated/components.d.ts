071bfbca8fd269e962a2d31b4767fd1672a5ca45
Gokul Surya <gokulsurya.gs@gmail.com>
2024-06-11 14:30:23 +0530

    feat: :sparkles: updated home-page api schema
    added contents in home-page api and tested in postman

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,36 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ComponentLink extends Schema.Component {
+  collectionName: 'components_component_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    text: Attribute.String;
+    link: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero-Section';
+    description: '';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.String;
+    image: Attribute.Media<'images'>;
+    link: Attribute.Component<'component.link'>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'component.link': ComponentLink;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
 }
