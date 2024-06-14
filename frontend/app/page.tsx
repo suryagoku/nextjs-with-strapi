bb1ac832ad38ca4438f0a92d7dc07a751bf60c80
Gokul Surya <gokulsurya.gs@gmail.com>
2024-06-14 13:44:15 +0530

    feat: :sparkles: HeroSection Component
    created a heroSection compoents with hard coded data and passed props

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,28 @@
-import { Button } from '@/components/ui/button'
+import HeroSection from '@/components/custom/HeroSection'
+import qs from 'qs'
+
+const homePageQuery = qs.stringify({
+  populate: {
+    content: {
+      populate: {
+        image: {
+          fields: ['url', 'alternativeText'],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+})
 
 const getStrapiData = async (path: string) => {
-  const baseUrl = 'http://localhost:1337/api'
+  const baseUrl = 'http://localhost:1337/api/'
+  const url = new URL(path, baseUrl)
+  url.search = homePageQuery
+
   try {
-    const response = await fetch(baseUrl + path, { cache: 'no-store' })
+    const response = await fetch(url.href, { cache: 'no-store' })
     const data = await response.json()
     return data
   } catch (error) {
@@ -12,13 +31,14 @@ const getStrapiData = async (path: string) => {
 }
 
 export default async function Home() {
-  const strapiData = await getStrapiData('/home-page')
-  console.log(strapiData)
-  const { title, description } = strapiData.data.attributes
+  const strapiData = await getStrapiData('home-page')
+  const { content } = strapiData?.data?.attributes
   return (
     <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
+      {/* <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p> */}
+      <h1>Home page</h1>
+      <HeroSection data={content[0]} />
     </main>
   )
 }
